7dd2161812e33071e52c25ce49f170fc3f5d8679
makrovan <makrov_an@icloud.com>
2023-10-26 13:43:06 +0800

    fixed bug

--- M6/src/shared/Post/CommentFormContainer/CommentFormContainer.tsx
+++ M6/src/shared/Post/CommentFormContainer/CommentFormContainer.tsx
@@ -2,13 +2,13 @@ import React, {ChangeEvent, FormEvent, useEffect, useRef, useState} from 'react'
 import {CommentForm} from "./CommentForm";
 
 interface ICommentFormContainerProps {
-  // postId: string;
+  postId: string;
   isCommentHide: boolean;
   helloText: string;
   onClose?: () => void;
 }
 
-export function CommentFormContainer({ isCommentHide, helloText, onClose}: ICommentFormContainerProps) {
+export function CommentFormContainer({ postId, isCommentHide, helloText, onClose}: ICommentFormContainerProps) {
   const ref = useRef<HTMLTextAreaElement>(null);
   const [value, setValue] = useState(helloText)
 
