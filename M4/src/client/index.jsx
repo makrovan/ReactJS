e695247f4b78566938855b0248b48545395e2d6c
makrovan <makrov_an@icloud.com>
2023-11-13 19:16:58 +0800

    empty folders deleted

--- M4/src/client/index.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as ReactDom from "react-dom";
-// import { Header } from "../shared/Header.tsx";
-import {App} from "../App";
-
-window.addEventListener("load", () => {
-  ReactDom.hydrate(<App />, document.getElementById("react_root"));
-});
--- M5/src/utils/ts/merge.ts
+++
@@ -1,6 +0,0 @@
-export function merge<O extends object>(obj1: O) {
-    return <O extends object>(obj2: O) => ({
-       ...obj1,
-       ...obj2
-    });
-}
